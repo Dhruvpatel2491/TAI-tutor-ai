29f8138302901062cf43431ebe90192d94336e60
dhruv_patel <dpatel45@pride.hofstra.edu>
2025-11-17 00:30:55 -0500

    remove test

--- test/data/CSC15/Week1/ex_trunc_ceil_round.py
+++
@@ -1,5 +0,0 @@
-import math
-
-print("Ceil function:",  math.ceil(4.2), math.ceil(4.6), math.ceil(5.0))
-print("Trunc function:", math.trunc(4.2), math.trunc(4.6), math.trunc(5.0))
-print("Round function:", round(4.2), round(4.55), round(5.0))
